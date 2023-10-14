ea9d9a8fd0641b2c3507dfa9d298836823a7d651
Igor Beljutins <igor.beljutins@geotogether.com>
2023-10-14 17:58:38 +0100

    moved functions into the lib

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
-///
-const SEQ_A070199_1M_NUMBER: u32 = 1_999; // Number of palindromes of length <= 1,000,000 (https://oeis.org/A070199)
-const MAX_LIMIT_NUMBER: u32 = 1_000_000; // Hardcoded requested limit
-
-///
-pub fn is_palindrome(n: u32) -> bool {
-    if n > MAX_LIMIT_NUMBER {
-        panic!("Number must not be greater than 1,000,000, got {}.", n);
-    }
-    let digits = n.to_string();
-    let front = digits.chars();
-    let back = front.clone().rev();
-    front.eq(back)
-}
-
-///
-pub fn first_n_palindromes(n: u32) -> Vec<u32> {
-    (0..MAX_LIMIT_NUMBER)
-        .filter(|x| is_palindrome(*x))
-        .take(
-            if n > SEQ_A070199_1M_NUMBER {
-                println!( "With {} numbers the palindrome number will exceed 1,000,000 limit.\nSo we only provide the numbers below that.", n);
-                SEQ_A070199_1M_NUMBER
-            } else {
-                n
-            } as usize)
-        .collect()
-}
+use bs_crate;
 
 fn main() {
     let x = 121;
-    let is_palindrome = is_palindrome(x);
+    let is_palindrome = bs_crate::is_palindrome(x);
     println!("{x} is a palindrome: {is_palindrome}");
 
-    let first_10_palindromes = first_n_palindromes(SEQ_A070199_1M_NUMBER + 1);
+    let first_10_palindromes = bs_crate::first_n_palindromes(10);
     for x in first_10_palindromes {
         println!("{x} is a palindrome");
     }
