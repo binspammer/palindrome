605bc9e71d09dc07c5838fd82b6a3098ef6f9fde
Igor Beljutins <igor.beljutins@geotogether.com>
2023-10-14 13:17:53 +0100

    refactored again

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 ///
 ///
 pub fn is_palindrome(n: u32) -> bool {
-    let s = n.to_string();
-    let digits: Vec<_> = s.chars().collect();
-    let lenght = digits.len();
-    for i in 0..(lenght / 2) {
-        if &digits[i] != &digits[lenght - 1 - i] {
+    let num = n.to_string();
+    let mut it = num.chars();
+    while let (Some(front), Some(back)) = (it.next(), it.next_back()) {
+        if front != back {
             return false;
         }
     }
@@ -14,10 +13,7 @@ pub fn is_palindrome(n: u32) -> bool {
 
 ///
 pub fn first_n_palindromes(n: usize) -> Vec<u32> {
-    (0..)
-        .filter(|x| is_palindrome(*x))
-        .take(n)
-        .collect::<Vec<u32>>()
+    (0..).filter(|x| is_palindrome(*x)).take(n).collect()
 }
 
 ///
@@ -27,7 +23,6 @@ fn main() {
     println!("{x} is a palindrome: {is_palindrome}");
 
     let first_10_palindromes = first_n_palindromes(10);
-
     for x in first_10_palindromes {
         println!("{x} is a palindrome");
     }
