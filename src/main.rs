99a62de9ddc180b7df52501fce9744a68e3ff77e
Igor Beljutins <igor.beljutins@geotogether.com>
2023-10-14 13:42:38 +0100

    refactored again

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
 ///
-///
-pub fn is_palindrome(n: u32) -> bool {
-    let num = n.to_string();
-    let mut it = num.chars();
-    while let (Some(front), Some(back)) = (it.next(), it.next_back()) {
-        if front != back {
-            return false;
-        }
-    }
-    true
-}
 
 ///
-pub fn is_palindrome1(n: u32) -> bool {
+pub fn is_palindrome(n: u32) -> bool {
     let num = n.to_string();
     let front = num.chars();
     let back = front.clone().rev();
@@ -30,16 +19,8 @@ fn main() {
     let is_palindrome = is_palindrome(x);
     println!("{x} is a palindrome: {is_palindrome}");
 
-    let first_10_palindromes = first_n_palindromes(10);
+    let first_10_palindromes = first_n_palindromes(20);
     for x in first_10_palindromes {
         println!("{x} is a palindrome");
     }
-
-    println!("compare 2 itarators:");
-    let numb = 11;
-    let txt = numb.to_string();
-    let chrs = txt.chars();
-    let chrsrv = chrs.clone().rev();
-    let res = chrs.eq(chrsrv);
-    println!("{res}");
 }
